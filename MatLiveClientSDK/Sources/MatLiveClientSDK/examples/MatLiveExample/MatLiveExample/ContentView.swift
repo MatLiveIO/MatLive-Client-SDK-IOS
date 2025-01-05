bd628ea6406f3383eb74f0d7686a218467264d37
Anas Amer <164742706+anasmatn@users.noreply.github.com>
2025-01-05 15:58:36 +0300

    create project and add sdk

---
+++ MatLiveClientSDK/Sources/MatLiveClientSDK/examples/MatLiveExample/MatLiveExample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MatLiveExample
+//
+//  Created by anas amer on 05/01/2025.
+//
+
+import SwiftUI
+import MatLiveClientSDK
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
